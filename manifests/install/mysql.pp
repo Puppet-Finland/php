1522b14003f6c6ba39a6cfd3f44c8dbd31828f3d
Samuli Seppänen <samuli.seppanen@gmail.com>
2014-11-20 15:15:44 +0200

    Removed an invisible character from php::mysql::install
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- manifests/install/mysql.pp
+++ manifests/install/mysql.pp
@@ -8,7 +8,7 @@ class php::install::mysql {
     include php::params
 
     package { 'php-php5-mysql':
-        name => $php::params::php_mysql_package_name,
+        name => $::php::params::php_mysql_package_name,
         ensure => installed,
         require => Class['php::install'],
     }        
