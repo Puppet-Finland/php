41d413f359b8de449ba1b216c0b5392810a66195
Samuli Seppänen <samuli@openvpn.net>
2016-06-09 14:17:40 +0000

    Simplify PHP library/package management
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

---
+++ manifests/package.pp
@@ -0,0 +1,26 @@
+#
+# == Define: php::package
+#
+# Install a php package, usually a library
+#
+# == Parameters
+#
+# [*ensure*]
+#   Status of the package. Valid values 'present', 'absent' and 'latest'.
+#   Defaults to 'present'.
+# [*title*]
+#   The resource $title is used as the package name. It is typically derived
+#   from the variables in $::php::params.
+#
+define php::package
+(
+    Enum['present', 'absent', 'latest'] $ensure = 'present'
+)
+{
+
+    # We use this function to minimize the risk of duplicate resource
+    # definitions when using "external" puppet modules
+    ensure_resource('package', "php-${title}", {'ensure'  => $ensure,
+                                                'name'    => $title,
+                                                'require' => Class['php::install'] })
+}
