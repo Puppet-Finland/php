d2129e14bef014d61c0d716ad5e893a7a694d05c
Petri Lammi <petri.lammi@tietoteema.fi>
2019-09-09 17:14:14 +0300

    Fix typo.
    Signed-off-by: Petri Lammi <petri.lammi@puppeteers.fi>

--- manifests/mcrypt.pp
+++ manifests/mcrypt.pp
@@ -4,7 +4,7 @@
 class php::mcrypt inherits php::params {
 
   # if php is newer or equal to 7.1, mcrypt is deprecated 
-  $php_version = regsubst($::php::params::base_name, 'php','')
+  $php_version = regsubst($::php::params::basename, 'php','')
   $max_version = '7.1'
 
   if ! $php_version or (versioncmp($php_version, $max_version) < 0) {
