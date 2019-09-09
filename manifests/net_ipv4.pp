45a24e8d99a918299c1dd1a2e510b071ad3015ff
Petri <petri.lammi@puppeteers.net>
2019-09-09 17:32:01 +0300

    Do no try to install net_ipv4 on bionic.
    Signed-off-by: Petri <petri.lammi@puppeteers.net>

--- manifests/net_ipv4.pp
+++ manifests/net_ipv4.pp
@@ -2,5 +2,8 @@
 # == Class: php::net_ipv4
 #
 class php::net_ipv4 inherits php::params {
+
+  unless $facts['lsbdistcodename'] == 'bionic' {
     php::package { $::php::params::php_net_ipv4_package_name: }
+  }
 }
